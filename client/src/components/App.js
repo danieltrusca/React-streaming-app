6d1255f8dedbefc549f96d440a4c40e2a7256153
danieltrusca <truscadani@yahoo.com>
2021-11-25 19:46:53 +0200

    basic scaffolding components and basic routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// import components
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+
+import Header from './Header';
+import NoMatch from './NoMatch';
 
 const App = () => {
     return (
-        <div>
-            App
+        <div className="ui container">
+            
+            <BrowserRouter>
+                <>
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" component={StreamCreate} />
+                        <Route path="/streams/edit" component={StreamEdit} />
+                        <Route path="/streams/delete" component={StreamDelete} />
+                        <Route path="/streams/show" component={StreamShow} />
+                        <Route path="*" component={NoMatch} />
+                    </Switch>
+                    
+                </>
+            </BrowserRouter>
         </div>
     )
 }
